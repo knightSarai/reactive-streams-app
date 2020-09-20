5140a58ab268d0787edde48a657f74804a2dc1d9
knightSarai <knight.sarai.dev@gmail.com>
2020-09-20 17:35:44 +0300

    finish the app

--- reactive-streams/src/components/streams/StreamShow.js
+++ reactive-streams/src/components/streams/StreamShow.js
@@ -1,25 +1,60 @@
-import React, {useEffect} from 'react';
+import React, {Component, createRef} from 'react';
 import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions'
 
-function StreamShow(props) {
-    const {match, fetchStream, stream} = props;
-    
-    useEffect(()=> {
-        const {id} = match.params
-        fetchStream(id)
-    }, [match, fetchStream]);
-
-    if (!stream) {
-        return <h1>Loading..</h1>
+class StreamShow extends Component {
+    constructor(props) {
+        super(props);
+        this.videoRef = createRef()
     }
-    return (
-        <div>
-            <video />
-            Stream {stream.id}
-        </div>
-    )
+
+    componentDidMount() {
+        const {match, fetchStream} = this.props;
+        const {id} = match.params;
+        fetchStream(id);
+        this.buildPlayer();
+    }
+    
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer = () => {
+        const {stream, match} = this.props;
+        const {id} = match.params;
+        if (this.player || !stream) {
+            return;
+        }
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load()
+    }
+    
+    render() {
+        const {stream} = this.props;
+        if (!stream) {
+            return <h1>Loading..</h1>
+        }
+        return (
+            <div style={{marginTop: "1rem"}}>
+                <video 
+                    ref={this.videoRef} 
+                    style={{width: "100%", height: "85vh"}} 
+                    controls
+                />
+                <h1>Stream {stream.title}</h1>
+            </div>
+        )
+    }
+   
 }
 
 const mapStateToProps = (state, ownProps) => {
