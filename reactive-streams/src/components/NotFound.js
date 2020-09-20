c6bcc1024fd17cac3a79d377711e5ec6f4ed4606
knightSarai <knight.sarai.dev@gmail.com>
2020-09-20 15:19:38 +0300

    stream show & style not found page

--- reactive-streams/src/components/NotFound.js
+++ reactive-streams/src/components/NotFound.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+
+const styles = makeStyles(theme=>({
+    root: {
+        textAlign: "center",
+        marginTop: "2rem"
+        
+    }
+  }));
 
 export default function NotFound() {
+    const classes = styles();
     return (
         <div>
-            Not Found
+            <Typography variant="h1" className={classes.root}>
+                Page Not Found :(
+            </Typography>
         </div>
     )
 }
--- reactive-streams/src/components/streams/StreamShow.js
+++ reactive-streams/src/components/streams/StreamShow.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions'
 
-export default function StreamShow() {
+function StreamShow(props) {
+    const {match, fetchStream, stream} = props;
+    
+    useEffect(()=> {
+        const {id} = match.params
+        fetchStream(id)
+    }, [match, fetchStream]);
+
+    if (!stream) {
+        return <h1>Loading..</h1>
+    }
     return (
         <div>
-            StreamShow
+            Stream {stream.id}
         </div>
     )
 }
+
+const mapStateToProps = (state, ownProps) => {
+    const {id} = ownProps.match.params;
+    return {
+        stream: state.streams[id], 
+        isSignedIn: state.auth.isSignedIn, 
+        oauth: state.auth.oauth
+    };
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamShow)
